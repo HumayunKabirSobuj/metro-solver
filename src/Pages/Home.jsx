df491fc976595facc9c846247f2e1d8428c35ba1
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-12-14 23:26:14 +0600

    review section complete

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import Banner from '../HomePageComponent/Banner';
 import HowToWorks from '../HomePageComponent/HowToWorks';
+import ReviewsSlider from '../HomePageComponent/ReviewsSlider';
 import Navbar from '../utils/Navbar';
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
         <Navbar></Navbar>
         <Banner></Banner>
         <HowToWorks />
+        <ReviewsSlider></ReviewsSlider>
       </div>
     </div>
   );
