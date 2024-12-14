cb4c2a8b56b1b6b1ccb9d4fea8539d9357ac3176
Md. Humayun Kabir Sobuj <humayunkabirgithub@gmail.com>
2024-12-14 17:05:49 +0600

    folder name changed

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,12 @@
+
+const Home = () => {
+  return (
+    <div className='bg-custom-gradient'>
+      <div className='max-w-[1240px] mx-auto h-screen py-5'>
+        <h1 className='text-3xl font-bold '>Home</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Home
