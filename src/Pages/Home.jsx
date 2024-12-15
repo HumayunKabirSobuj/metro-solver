fd9be24ec6be4c8c8c204cd35b18fcaef0ee8e50
Sahed96 <ninja.sahedkhan@gmail.com>
2024-12-15 13:27:08 +0600

    footer added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,6 +6,7 @@ import Navbar from '../utils/Navbar';
 import LatestWork from '../HomePageComponent/LatestWork';
 import OurService from '../HomePageComponent/OurService';
 import ContactForm from '../HomePageComponent/ContactForm';
+import Footer from '../utils/Footer';
 
 const Home = () => {
   return (
@@ -20,6 +21,7 @@ const Home = () => {
         <ReviewsSlider></ReviewsSlider>
         <OurService></OurService>
         <ContactForm></ContactForm>
+        <Footer />
       </div>
     </div>
   );
